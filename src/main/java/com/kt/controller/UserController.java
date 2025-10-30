839db3a693a5c9b1b615aad6f889bf7293518ee4
yeilkk <yeil99@naver.com>
2025-10-30 11:52:43 +0900

    feat: swagger 문서화 완료

--- src/main/java/com/kt/controller/UserController.java
+++ src/main/java/com/kt/controller/UserController.java
@@ -9,16 +9,32 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kt.dto.UserCreateRequest;
 import com.kt.service.UserService;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "유저", description = "유저 관련 API")
 public class UserController {
 
 	private final UserService userService;
 
+	/* API 문서화 크게 2가지 방식 존제
+	1. Swagger
+	장점: UI 이쁨, 어노테이션 기반으로 작성이 쉬움
+	단점: 프로덕션 코드에 Swagger 관련 어노테이션이 존재함 -> SRP 위분 (Controller), 코드가 복잡해지고 유지보수가 힘듦
+	2. RestDocs
+	장점: 프로덕션 코드에 침법이 없음, 신뢰성 있음
+	단점: UI 이쁘지 않음. 테스트 기반이라 문서 작성에 시간이 걸림
+	*/
 
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "400", description = "유효성 검사 실패"),
+		@ApiResponse(responseCode = "500", description = "서버 에러 - 백엔드 문의 바랍니다.")
+	})
 	@PostMapping("/users")
 	@ResponseStatus(HttpStatus.CREATED)
 	// json 형태의 body에 담겨서 post 요청으로 /users로 들어오면
