2096ec6e7c82ab656588ce63675b0ee8177ca9e4
yeilkk <yeil99@naver.com>
2025-10-30 09:55:46 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/kt/dto/UserCreateRequest.java
@@ -0,0 +1,13 @@
+package com.kt.dto;
+
+import java.time.LocalDate;
+
+// loginId, password, name, birthday(YYYY-mm-dd)
+public record UserCreateRequest(
+	String loginId,
+	String password,
+	String name,
+	LocalDate birthday
+) {
+
+}
---
+++ src/main/java/com/kt/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package com.kt.repository;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kt.domain.User;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepository {
+
+	private final JdbcTemplate jdbcTemplate;
+
+	public void save(User user){
+		// 서비스에서 dto를 도메인으로 바꾼 다음에 전달
+		var sql = "INSERT INTO MEMBER (loginId, password, name, birthday) VALUES (?, ?, ?, ?)";
+
+		jdbcTemplate.update(sql, user.getLoginId(), user.getPassword(), user.getName(), user.getBirthday());
+		System.out.println("Saved User: "+user.toString());
+	}
+}
