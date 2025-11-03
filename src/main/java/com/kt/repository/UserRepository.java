fd234779fbc85dc7328271f6d55ea3111f5b5541
yeilkk <yeil99@naver.com>
2025-11-03 10:09:17 +0900

    feat: 비밀번호 변경 서비스 및 유저 id 검사 메소드 추가

--- src/main/java/com/kt/repository/UserRepository.java
+++ src/main/java/com/kt/repository/UserRepository.java
@@ -67,4 +67,9 @@ public class UserRepository {
 		var sql = "UPDATE member SET password = ? WHERE id = ?";
 		jdbcTemplate.update(sql, password, id);
 	}
+
+	public boolean existsById(int id){
+		var sql = "SELECT EXISTS (SELECT id FROM MEMBER WHERE id = ?)";
+		return Boolean.TRUE.equals(jdbcTemplate.queryForObject(sql, Boolean.class, id));
+	}
 }
--- src/main/java/com/kt/service/UserService.java
+++ src/main/java/com/kt/service/UserService.java
@@ -19,7 +19,7 @@ public class UserService {
 	public void create(UserCreateRequest request) {
 		System.out.println(request.toString());
 		var newUser = new User(
-			userRepository.selectMaxId()+1,
+			userRepository.selectMaxId() + 1,
 			request.loginId(),
 			request.password(),
 			request.name(),
@@ -34,7 +34,22 @@ public class UserService {
 	}
 
 	// TODO: 아이디 중복 검사 만들기
-	public boolean isDuplicateLoginId(String loginId){
+	public boolean isDuplicateLoginId(String loginId) {
 		return userRepository.existsByLoginId(loginId);
+	}
+
+	public void changePassword(int id, String oldPassword, String password) {
+		// TODO: 존재하지 않는 경우 유저를 찾을 수 없다는 예외 처리
+		// 서비스 입장에서는 id 값이 외부에서 들어오는 값
+		// 실제로 DB에 유저가 존재하는지 검사
+		// 존재 O 업데이트
+		// 존재 X 예외 처리
+		if(!userRepository.existsById(id)){
+			throw new IllegalArgumentException("존재하지 않는 회원입니다.");
+		}
+		if(oldPassword.equals(password)) {
+			throw new IllegalArgumentException("기존 비밀번호와 동일한 비밀번호로 변경할 수 없습니다.");
+		}
+		userRepository.updatePassword(id, password);
 	}
 }
