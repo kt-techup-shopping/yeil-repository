14c015bd9db63c7136318ba881db2fb23da5df9f
yeilkk <yeil99@naver.com>
2025-11-06 12:13:39 +0900

    [OPS-25] feat: 엔티티 추상화

---
+++ src/main/java/com/kt/domain/BaseEntity.java
@@ -0,0 +1,15 @@
+package com.kt.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public abstract class BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected Long id;
+	protected LocalDateTime createdAt;
+	protected LocalDateTime updatedAt;
+}
