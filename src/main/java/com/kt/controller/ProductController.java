038ab1a256c7a4503e2627cb80f4a02137359eed
yeilkk <yeil99@naver.com>
2025-11-06 16:17:02 +0900

    [OPS-30] feat: product 삭제 api

--- src/main/java/com/kt/controller/ProductController.java
+++ src/main/java/com/kt/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.kt.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -42,5 +43,11 @@ public class ProductController {
 	@ResponseStatus(HttpStatus.OK)
 	public void update(@PathVariable Long id, @RequestBody @Valid ProductUpdateRequest request) {
 		productService.update(id, request.name(), request.price(), request.stock(), request.status());
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void delete(@PathVariable Long id) {
+		productService.delete(id);
 	}
 }
