5cbdeef08727967172e4f4961687c632567aa047
yeilkk <yeil99@naver.com>
2025-11-06 15:43:16 +0900

    [OPS-28] feat: product 생성 api

---
+++ src/main/java/com/kt/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.kt.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kt.dto.ProductCreateRequest;
+import com.kt.dto.UserCreateRequest;
+import com.kt.service.ProductService;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/products")
+@Tag(name = "상품", description = "상품 관련 API")
+@ApiResponses(value = {
+	@ApiResponse(responseCode = "400", description = "유효성 검사 실패"),
+	@ApiResponse(responseCode = "500", description = "서버 에러 - 백엔드 문의 바랍니다.")
+})
+public class ProductController {
+
+	private final ProductService productService;
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void create(@RequestBody @Valid ProductCreateRequest request) {
+		productService.create(request);
+	}
+}
